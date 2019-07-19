bf4755adfbb25e63fc8134574d2beb5deb012180
thesachinmittal <sachin.narenmittal@gmail.com>
2019-07-19 23:58:38 +0530

    Final contract initiated

---
+++ contracts/Dealer.sol
@@ -0,0 +1,23 @@
+ pragma solidity ^0.5.0;
+ 
+ ///@title fun.fair _Consensys-bootcamp-final-project
+ ///@author Sachin Mittal
+ ///@notice Contract is built following the smart contract design pattern
+ ///@dev Uses hybrid commit-reveal method + block hash RNG. Provides provably fair gambling, and arbitrary high bets
+
+ contract Dealer{
+
+///@title Contract properties - Monolithic, 
+///@dev For any queries, generate an issue, or email at " "
+
+
+// * Ethereum smart contract, deployed at "given address". */
+
+/// *** STATE VARIABLES 
+
+uint constant HOUSE_COMM_PER = 1;
+uint constant HOUSE_MIN_COMM = 0.0003; //Error: try explicit conversion tomorrow
+
+// Bets lower than this amount do not participate (and are not deducted jackpot fees).
+
+ }
