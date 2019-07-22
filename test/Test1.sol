29d8ec201b4e070d88879cced2af7a0f2f8c3315
thesachinmittal <sachin.narenmittal@gmail.com>
2019-07-22 22:12:45 +0530

    Test cases for game acceptance

--- test/Test1.sol
+++ test/Test1.sol
@@ -2,30 +2,30 @@
 pragma solidity ^0.5.0;
 
 import "truffle/Assert.sol";
-//import "truffle/DeployedAddresses.sol";
+
 import "../contracts/GamesOnStakes.sol";
 import "../contracts/StringSupport.sol";
 
 contract TestGamesOnStakes {
-    GamesOnStakes games;
+    GamesOnStakes gamesInstance;
 
     constructor() public {
-        games = GamesOnStakes(DeployedAddresses.GamesOnStakes());
+        gamesInstance = GamesOnStakes(DeployedAddresses.GamesOnStakes());
     }
 
     function testInitiallyEmpty() public {
-        Assert.equal(games.getOpenGames().length, 0, "The games array should be empty at the begining");
+        Assert.equal(gamesInstance.getOpenGames().length, 0, "The games array should be empty at the begining");
     }
 
     function testHashingFunction() public {
-        string memory hash1 = games.saltedHash(123, "my salt goes here");
-        string memory hashA = LibString.saltedHash(123, "my salt goes here");
+        string memory hash1 = gamesInstance.saltedHash(123, "my salt goes here");
+        string memory hashA = StringSupport.saltedHash(123, "my salt goes here");
 
-        string memory hash2 = games.saltedHash(123, "my salt goes 2 here");
-        string memory hashB = LibString.saltedHash(123, "my salt goes 2 here");
+        string memory hash2 = gamesInstance.saltedHash(123, "my salt goes 2 here");
+        string memory hashB = StringSupport.saltedHash(123, "my salt goes 2 here");
 
-        string memory hash3 = games.saltedHash(234, "my salt goes here");
-        string memory hashC = LibString.saltedHash(234, "my salt goes here");
+        string memory hash3 = gamesInstance.saltedHash(234, "my salt goes here");
+        string memory hashC = StringSupport.saltedHash(234, "my salt goes here");
 
         Assert.isNotEmpty(hash1, "Salted hash should be a valid string");
 
@@ -48,16 +48,17 @@ contract TestGamesOnStakes {
         uint lastTransaction1;
         uint lastTransaction2;
 
-        string memory hash = games.saltedHash(123, "my salt goes here");
+        string memory hash = gamesInstance.saltedHash(123, "my salt goes here");
+        uint32 gameIdx = gamesInstance.createGame(hash, "Sachin");
+        
         Assert.equal(uint(gameIdx), 0, "The first game should have index 0");
         
-        uint32 gameIdx = games.createGame(hash, "John");
-        uint32[] memory openGames = games.getOpenGames();
+        uint32[] memory openGames = gamesInstance.getOpenGames();
   
         Assert.equal(openGames.length, 1, "One game should have been created");
         
 
-        (cells, status, amount, created, nick1, nick2) = games.getGameInfo(gameIdx);
+        (cells, status, amount, created, nick1, nick2) = gamesInstance.getGameInfo(gameIdx);
         Assert.equal(uint(cells[0]), 0, "The board should be empty");
         Assert.equal(uint(cells[1]), 0, "The board should be empty");
         Assert.equal(uint(cells[2]), 0, "The board should be empty");
@@ -70,14 +71,56 @@ contract TestGamesOnStakes {
         Assert.equal(uint(status), 0, "The game should not be started");
         Assert.equal(amount, 0, "The initial amount should be zero");
         //Assert.equal(created, 0, "Not created yet");
-        Assert.equal(nick1, "John", "The nick should be John");
+        Assert.equal(nick1, "Sachin", "The nick should be Sachin");
         Assert.isEmpty(nick2, "Nick2 should be empty");
 
-        (created, lastTransaction1, lastTransaction2) = games.getGameTimestamps(gameIdx);
+        (created, lastTransaction1, lastTransaction2) = gamesInstance.getGameTimestamps(gameIdx);
         Assert.isAbove(created, 0, "Creation date should be set");
         Assert.isAbove(lastTransaction1, 0, "The first player's transaction timestamp should be set");
         Assert.equal(lastTransaction2, 0, "The second player's transaction timestamp should be empty");
     }
+
+    function testGameAccepted() public{
+
+         uint8[9] memory cells;
+        uint8 status;
+        uint amount;
+        string memory nick1;
+        string memory nick2;
+        uint created;
+        uint lastTransaction1;
+        uint lastTransaction2;
+
+        uint32[] memory openGames = gamesInstance.getOpenGames();
+        Assert.equal(openGames.length, 1, "One game should be available");
+
+        gamesInstance.acceptGame(openGames[0], 0, "Sanchay");
+
+        openGames = gamesInstance.getOpenGames();
+        Assert.equal(openGames.length, 1, "One game should still exist");
+        Assert.equal(uint(openGames[0]), 0, "The game should still have index 0");
+
+        (cells, status, amount, nick1, nick2) = gamesInstance.getGameInfo(openGames[0]);
+        Assert.equal(uint(cells[0]), 0, "The board should be empty");
+        Assert.equal(uint(cells[1]), 0, "The board should be empty");
+        Assert.equal(uint(cells[2]), 0, "The board should be empty");
+        Assert.equal(uint(cells[3]), 0, "The board should be empty");
+        Assert.equal(uint(cells[4]), 0, "The board should be empty");
+        Assert.equal(uint(cells[5]), 0, "The board should be empty");
+        Assert.equal(uint(cells[6]), 0, "The board should be empty");
+        Assert.equal(uint(cells[7]), 0, "The board should be empty");
+        Assert.equal(uint(cells[8]), 0, "The board should be empty");
+        Assert.equal(uint(status), 0, "The game should not be started");
+        Assert.equal(amount, 0, "The initial amount should be zero");
+        Assert.equal(nick1, "Sachin", "The nick should be Sachin");
+        Assert.equal(nick2, "Sanchay", "The nick should be Sanchay");
+
+        (created, lastTransaction1, lastTransaction2) = gamesInstance.getGameTimestamps(openGames[0]);
+        Assert.isAbove(created, 0, "Creation date should be set");
+        Assert.isAbove(lastTransaction1, 0, "The first player's transaction timestamp should be set");
+        Assert.isAbove(lastTransaction2, 0, "The second player's transaction timestamp should be set");
+
+    }
 }
-    
+
 
