8232689393ea8750006fd583cd6d0bf32036aed8
thesachinmittal <sachin.narenmittal@gmail.com>
2019-07-22 11:33:30 +0530

    Moving ahead with TDD

--- contracts/GamesOnStakes.sol
+++ contracts/GamesOnStakes.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.5.0;
  ///@notice Contract is built following the smart contract design pattern
  ///@dev 
 
+ import "./StringSupport.sol";
+
 contract GamesOnStakes {
 
 ///@title Contract properties - Monolithic, 
@@ -28,7 +30,7 @@ struct Game
     // 0 => not started, 1 => player 1, 2 => player 2
     // 10 => draw, 11 => player 1 wins, 12 => player 2 wins
 
-    uint status;
+    uint8 status;
           
     uint amount;                        // amount of money each user has sent
 
@@ -42,17 +44,106 @@ struct Game
   }
 
     uint32[] openGames;                    // list of active games' id's
-    mapping(uint32 => Game) gamesData; // data containers
+    mapping(uint32 => Game) gamesData;     // data containers
 
-    uint32 nextGameIdx;                
+    uint32 nextGameIdx;
     uint16 public timeout;
 
 constructor() public {
     }
 
+    // EVENTS
+
     event GameCreated(uint32 indexed gameIdx);
     event GameAccepted(uint32 indexed gameIdx);
     event GameStarted(uint32 indexed gameIdx);
     event GameEnded(uint32 indexed gameIdx);
+    event PositionMarked(uint32 indexed gameIdx);
+
+    // Member Functions
+
+    //Callable 
+
+    function getOpenGames() 
+    public 
+    view 
+    returns (uint32[] memory){}
+
+
+    function getGameInfo(uint gameIdx)
+    public
+    view 
+    returns (uint8[9] memory cells, uint8 status, uint amount, string memory nick1, string memory nick2) {
+        return (cells, status, amount, nick1, nick2);
+    }
+
+    // Operations
+
+    function createGame(string memory randomNumberHash) public payable returns (address) {
+        emit GameCreated(nextGameIdx);
+    }
+
+    function acceptGame(address gameId, uint8 randomNumber) public payable {
+        emit GameAccepted(nextGameIdx);
+    }
+
+    function confirmGame(address gameId, uint8 originalRandomNumber, bytes32 originalSalt) public {
+        emit GameStarted(nextGameIdx);
+    }
+
+    function markPosition(address gameId, uint8 cell) public {
+        emit PositionMarked(nextGameIdx);
+    }
+    
+
+    function withdraw(address gameId) public {
+        emit GameEnded(nextGameIdx);
+    }
+   
+
+    // Imported from library - public helper function
+    function saltedHash(uint8 randomNumber, string memory salt) public pure returns (bytes32) {
+        return StringSupport.saltedHash(randomNumber, salt);
+    }
+
+    // Fallback function
+
+    function () external payable {
+        revert();
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+    // mapping (address => uint) balances;
+
+    // event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    // constructor() public {
+    //     balances[msg.sender] = 10000;
+    // }
+
+    // function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+    //     if (balances[msg.sender] < amount) return false;
+    //     balances[msg.sender] -= amount;
+    //     balances[receiver] += amount;
+    //     emit Transfer(msg.sender, receiver, amount);
+    //     return true;
+    // }
+
+    // function getBalanceInEth(address addr) public view returns(uint){
+    //     return ConvertLib.convert(getBalance(addr),2);
+    // }
+
+    // function getBalance(address addr) public view returns(uint) {
+    //     return balances[addr];
+    // }
---
+++ test/Test1.sol
@@ -0,0 +1,40 @@
+// test/Test1.sol
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/GamesOnStakes.sol";
+import "../contracts/StringSupport.sol";
+
+contract TestGamesOnStakes {
+    GamesOnStakes games;
+
+    constructor() public {
+        games = GamesOnStakes(DeployedAddresses.GamesOnStakes());
+    }
+
+    function testInitiallyEmpty() public {
+        Assert.equal(games.getOpenGames().length, 0, "The games array should be empty at the begining");
+    }
+
+    function testHashingFunction() public {
+        string memory hash1 = games.saltedHash(123, "my salt goes here");
+        string memory hashA = LibString.saltedHash(123, "my salt goes here");
+
+        string memory hash2 = games.saltedHash(123, "my salt goes 2 here");
+        string memory hashB = LibString.saltedHash(123, "my salt goes 2 here");
+
+        string memory hash3 = games.saltedHash(234, "my salt goes here");
+        string memory hashC = LibString.saltedHash(234, "my salt goes here");
+
+        Assert.isNotEmpty(hash1, "Salted hash should be a valid string");
+
+        Assert.equal(hash1, hashA, "Hashes should match");
+        Assert.equal(hash2, hashB, "Hashes should match");
+        Assert.equal(hash3, hashC, "Hashes should match");
+
+        Assert.notEqual(hash1, hash2, "Different salt should produce different hashes");
+        Assert.notEqual(hash1, hash3, "Different numbers should produce different hashes");
+        Assert.notEqual(hash2, hash3, "Different numbers and salt should produce different hashes");
+    }
+}
