19b95edb9586bedaee531f916c3f64cc549feca7
thesachinmittal <sachin.narenmittal@gmail.com>
2019-07-22 18:45:03 +0530

    Game creation functionality added

--- contracts/GamesOnStakes.sol
+++ contracts/GamesOnStakes.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.5.0;
 
-///@title fun.fair _Consensys-bootcamp-final-project
+ ///@title fun.fair _Consensys-bootcamp-final-project
  ///@author Sachin Mittal
  ///@notice Contract is built following the smart contract design pattern
- ///@dev 
+ ///@dev StringSupport library is used for salted hash (commitment scheme)
 
  import "./StringSupport.sol";
 
 contract GamesOnStakes {
 
-///@title Contract properties - Monolithic, 
+///@title Contract properties - Monolithic, experimental, efficient (Use of library)
 ///@dev For any queries, generate an issue, or email at " "
 
 
@@ -22,7 +22,7 @@ struct Game
   {
 
     
-    uint32 openListIndex;            // position in openGames[]
+    uint index;            // position in openGames[]
     uint8[9] cells;  
 
     // [123 | 456 | 789] containing [0 => nobody, 1 => X, 2 => O]    
@@ -30,24 +30,24 @@ struct Game
     // 0 => not started, 1 => player 1, 2 => player 2
     // 10 => draw, 11 => player 1 wins, 12 => player 2 wins
 
-    uint8 status;
-          
+    uint8 status;                   
+    
+    uint created;                       // Timestamp
     uint amount;                        // amount of money each user has sent
 
-    address[2] players;
-    string[2] nicks;
+    address[2] players;                 // Player;s array
+    string[2] nicks;                    
     uint lastTransaction;               // timestamp => block number
     bool[2] withdrawn;
 
-    bytes32 creatorHash;
+    string creatorHash;                   
     uint guestRandomNumber;
   }
 
     uint32[] openGames;                    // list of active games' id's
     mapping(uint32 => Game) gamesData;     // data containers
 
-    uint32 nextGameIdx;
-    uint16 public timeout;
+    uint32 lastGameIdx;
 
 constructor() public {
     }
@@ -57,8 +57,10 @@ constructor() public {
     event GameCreated(uint32 indexed gameIdx);
     event GameAccepted(uint32 indexed gameIdx);
     event GameStarted(uint32 indexed gameIdx);
-    event GameEnded(uint32 indexed gameIdx);
+    
     event PositionMarked(uint32 indexed gameIdx);
+    event GameEnded(uint32 indexed gameIdx);
+
 
     // Member Functions
 
@@ -67,37 +69,73 @@ constructor() public {
     function getOpenGames() 
     public 
     view 
-    returns (uint32[] memory){}
+    returns (uint32[] memory){
+      return openGames;
+    }
 
 
-    function getGameInfo(uint gameIdx)
+    function getGameInfo(uint32 gameIdx)
     public
     view 
-    returns (uint8[9] memory cells, uint8 status, uint amount, string memory nick1, string memory nick2) {
-        return (cells, status, amount, nick1, nick2);
+    returns (uint8[9] memory cells, uint8 status, uint amount, uint created, string memory nick1, string memory nick2) {
+        return (
+
+        gamesData[gameIdx].cells,
+        gamesData[gameIdx].status,
+        gamesData[gameIdx].amount,
+        gamesData[gameIdx].created,
+        gamesData[gameIdx].nicks[0],
+        gamesData[gameIdx].nicks[1]
+        );
     }
+
+    function getGamePlayers(uint32 gameIdx) 
+    public 
+    view 
+    returns (address player1, address player2) {
+        return (
+            gamesData[gameIdx].players[0],
+            gamesData[gameIdx].players[1]
+        );
+    }
+
 
     // Operations
 
-    function createGame(string memory randomNumberHash) public payable returns (address) {
-        emit GameCreated(nextGameIdx);
+   function createGame(string memory randomNumberHash, string memory nick) 
+   public 
+   payable 
+   returns (uint32 gameIdx) {
+      gamesData[lastGameIdx].index = openGames.length;
+      gamesData[lastGameIdx].creatorHash = randomNumberHash;
+      gamesData[lastGameIdx].amount = msg.value;
+      gamesData[lastGameIdx].created = now;
+      gamesData[lastGameIdx].nicks[0] = nick;
+      gamesData[lastGameIdx].players[0] = msg.sender;
+      openGames.push(lastGameIdx);
+
+      gameIdx = lastGameIdx;
+      emit GameCreated(lastGameIdx);
+
+      lastGameIdx++;
+  }
+
+
+    function acceptGame(uint32 gameIdx, uint8 randomNumber) public payable {
+        emit GameAccepted(lastGameIdx+1);
     }
 
-    function acceptGame(address gameId, uint8 randomNumber) public payable {
-        emit GameAccepted(nextGameIdx);
+    function confirmGame(uint32 gameIdx, uint8 originalRandomNumber, bytes32 originalSalt) public {
+        emit GameStarted(lastGameIdx+1);
     }
 
-    function confirmGame(address gameId, uint8 originalRandomNumber, bytes32 originalSalt) public {
-        emit GameStarted(nextGameIdx);
-    }
-
-    function markPosition(address gameId, uint8 cell) public {
-        emit PositionMarked(nextGameIdx);
+    function markPosition(uint gameIdx, uint8 cell) public {
+        emit PositionMarked(lastGameIdx+1);
     }
     
 
     function withdraw(address gameId) public {
-        emit GameEnded(nextGameIdx);
+        emit GameEnded(lastGameIdx+1);
     }
    
 
@@ -122,28 +160,3 @@ constructor() public {
 
 
 
-
-
-    // mapping (address => uint) balances;
-
-    // event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    // constructor() public {
-    //     balances[msg.sender] = 10000;
-    // }
-
-    // function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-    //     if (balances[msg.sender] < amount) return false;
-    //     balances[msg.sender] -= amount;
-    //     balances[receiver] += amount;
-    //     emit Transfer(msg.sender, receiver, amount);
-    //     return true;
-    // }
-
-    // function getBalanceInEth(address addr) public view returns(uint){
-    //     return ConvertLib.convert(getBalance(addr),2);
-    // }
-
-    // function getBalance(address addr) public view returns(uint) {
-    //     return balances[addr];
-    // }
