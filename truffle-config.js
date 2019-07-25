ca56f7f010a1cdda4c61a4af75a10d29ecfe2af1
thesachinmittal <sachin.narenmittal@gmail.com>
2019-07-25 11:50:57 +0530

    !! Successfully deployed on ropsten !!

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "51dd41a5e1f642999384741bc86240e2";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+//var ropstenProvider = new HDWalletProvider(HDWalletProvider, "https://ropsten.infura.io/51dd41a5e1f642999384741bc86240e2");
 
 module.exports = {
   /**
@@ -60,14 +61,16 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    ropsten: {
+      provider: function(){
+        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/51dd41a5e1f642999384741bc86240e2`);
+      },
+      network_id: 3,       // Ropsten's id
+      gas: 4600000,        // Ropsten has a lower block limit than mainnet
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+     },
 
     // Useful for private networks
     // private: {
@@ -75,7 +78,7 @@ module.exports = {
       // network_id: 2111,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+    // },
 
   // Set default mocha options here, use special reporters etc.
   //mocha: {
@@ -97,3 +100,4 @@ module.exports = {
     //}
   //}
 }
+};
